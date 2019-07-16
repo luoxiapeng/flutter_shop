102df81c4ccac04d96067c9142d8039f2a35f436
luoxiapeng <949123073@qq.com>
2019-07-16 13:38:30 +0800

    增加首页

---
+++ lib/page/Index_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class IndexPage extends StatelessWidget {
+  const IndexPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('百信生活＋'),
+        ),
+      ),
+    );
+  }
+}
