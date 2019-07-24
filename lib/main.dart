41fcf9bee4ca11905cee77559cd14028cd176e47
luoxiapeng <949123073@qq.com>
2019-07-24 23:30:50 +0800

    使用dio库调用接口返回数据，渲染在页面上

--- lib/main.dart
+++ lib/main.dart
@@ -21,10 +21,11 @@ class MyApp extends StatelessWidget {
   void getHttp() async {
     try {
       Response response;
-      response = await Dio().get("https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=大胸妹");
-      print(response);
+      response = await Dio().get(
+          "https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=大胸妹");
+      return print(response);
     } catch (e) {
-      print(e)
+      return print(e);
     }
   }
 }
