bcd1f4501165765eeeca9352abd4c10f5cd3dbd8
luoxiapeng <949123073@qq.com>
2019-08-16 16:30:39 +0800

    调试了商品详情接口

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:provide/provide.dart';
 import './Provide/counter.dart';
 import './Provide/child_category.dart';
 import './Provide/category_goods_list.dart';
+import './Provide/details_info.dart';
 import './routers/routes.dart';
 import './routers/application.dart';
 
@@ -14,12 +15,14 @@ void main() {
   
   var childCategory= ChildCategory();
   var categoryGoodsListProvide= CategoryGoodsListProvide();
-  
+  var detailsInfoProvide=DetailsInfoProvide();
+
   var counter =Counter();
   var providers  =Providers();
   providers
     ..provide(Provider<ChildCategory>.value(childCategory))
     ..provide(Provider<CategoryGoodsListProvide>.value(categoryGoodsListProvide))
+    ..provide(Provider<DetailsInfoProvide>.value(detailsInfoProvide))
     ..provide(Provider<Counter>.value(counter));
 
   runApp(ProviderNode(child:MyApp(),providers:providers));
--- lib/page/details_page.dart
+++ lib/page/details_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provide/provide.dart';
+import '../Provide/details_info.dart';
+
 class DetailsPage extends StatelessWidget {
   // const DetailsPage({Key key}) : super(key: key);
   
@@ -7,8 +10,12 @@ class DetailsPage extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+    _getBackInfo(context);
     return Container(
       child: Text('${goodsId}'),
     );
+  }
+  void _getBackInfo(BuildContext context){
+    Provide.value<DetailsInfoProvide>(context).getGoodsInfo(goodsId);
   }
 }
